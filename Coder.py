038061297f9fa35a8df9c806dd00b9354d3eb6f7
Giao Pham <giaopham99@yahoo.com>
2020-05-07 15:16:43 -0400

    Added Encrypter

---
+++ Coder.py
@@ -0,0 +1,48 @@
+# Used to encode and decode data
+lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+         "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+         "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+special = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*",
+           "(", ")", "-", "_", "=", "+", "{", "[", "]", "}", "|", "\\", ":", ";", "\"", "'", ",", "<", ".", ">", "/", "?"]
+
+
+def encrypt(code, shift):
+    return code
+
+
+def eLower():
+    return 0
+
+
+def eUpper():
+    return 0
+
+
+def eNum():
+    return 0
+
+
+def eSpecial():
+    return 0
+
+
+def decrypt(code, shift):
+    return code
+
+
+def dLower():
+    return 0
+
+
+def dUpper():
+    return 0
+
+
+def dNum():
+    return 0
+
+
+def dSpecial():
+    return 0
