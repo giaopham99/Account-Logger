2faf5e518da3d6579a774a594e6bafb59ef0c84b
Giao Pham <giaopham99@yahoo.com>
2020-05-07 18:10:33 -0400

    Completed Coder

--- Coder.py
+++ Coder.py
@@ -1,48 +1,23 @@
 # Used to encode and decode data
-lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-         "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
-         "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-special = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*",
-           "(", ")", "-", "_", "=", "+", "{", "[", "]", "}", "|", "\\", ":", ";", "\"", "'", ",", "<", ".", ">", "/", "?"]
+characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+              "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+              "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*",
+              "(", ")", "-", "_", "=", "+", "{", "[", "]", "}", "|", "\\", ":", ";", "\"", "'", ",", "<", ".", ">", "/", "?"]
 
 
 def encrypt(code, shift):
-    return code
-
-
-def eLower():
-    return 0
-
-
-def eUpper():
-    return 0
-
-
-def eNum():
-    return 0
-
-
-def eSpecial():
-    return 0
+    encrypted = ""
+    for letter in code:
+        location = (characters.index(letter) + int(shift)) % len(characters)
+        encrypted = encrypted + characters[location]
+    return encrypted
 
 
 def decrypt(code, shift):
-    return code
-
-
-def dLower():
-    return 0
-
-
-def dUpper():
-    return 0
-
-
-def dNum():
-    return 0
-
-
-def dSpecial():
-    return 0
+    decrypted = ""
+    for letter in code:
+        location = (characters.index(letter) - int(shift))
+        if location < 0:
+            location = len(characters) - 1 + location
+        decrypted = decrypted + characters[location]
+    return decrypted
