1c5a23eec3983f7cff6a2ea14d235d11315dc960
Giao Pham <giaopham99@yahoo.com>
2020-05-08 11:57:52 -0400

    updated encrypt and decrypt

--- Coder.py
+++ Coder.py
@@ -2,22 +2,29 @@
 characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
               "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
               "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*",
-              "(", ")", "-", "_", "=", "+", "{", "[", "]", "}", "|", "\\", ":", ";", "\"", "'", ",", "<", ".", ">", "/", "?"]
+              "(", ")", "-", "_", "=", "+", "{", "[", "]", "}", "\\", ":", ";", "\"", "'", ",", "<", ".", ">", "/", "?"]
 
 
 def encrypt(code, shift):
     encrypted = ""
     for letter in code:
-        location = (characters.index(letter) + int(shift)) % len(characters)
-        encrypted = encrypted + characters[location]
+        if letter == " ":
+            encrypted = encrypted + "|"
+        else:
+            location = (characters.index(letter) +
+                        int(shift)) % len(characters)
+            encrypted = encrypted + characters[location]
     return encrypted
 
 
 def decrypt(code, shift):
     decrypted = ""
     for letter in code:
-        location = (characters.index(letter) - int(shift))
-        if location < 0:
-            location = len(characters) - 1 + location
-        decrypted = decrypted + characters[location]
+        if letter == "|":
+            decrypted = decrypted + " "
+        else:
+            location = (characters.index(letter) - int(shift))
+            if location < 0:
+                location = len(characters) - 1 + location
+            decrypted = decrypted + characters[location]
     return decrypted
