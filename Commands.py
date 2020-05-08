c9619f4d5ab78ec729ebd1c2750bdb815416c7af
Giao Pham <giaopham99@yahoo.com>
2020-05-08 11:48:13 -0400

    completed search and retrieve

--- Commands.py
+++ Commands.py
@@ -1,4 +1,5 @@
 from Coder import *
+import os
 
 
 def displaymenu():
@@ -11,8 +12,17 @@ def displaymenu():
     return input()
 
 
-def retrieve_account():
-    print("retrieved")
+def retrieve_account(file, shift):
+    account = input("What account are you looking for? ")
+    line = search(file, account)
+    if line == "0":
+        print("Error: Nickname for account does not exist.")
+    else:
+        line = line.split()
+        line = [decrypt(word, shift) for word in line]
+        print("Account: " + line[0])
+        print("Username: " + line[1])
+        print("Password: " + line[2])
 
 
 def addaccount(file, shift):
@@ -24,12 +34,57 @@ def addaccount(file, shift):
     file.write(userInput + " ")
     userInput = input("Password: ")
     userInput = encrypt(userInput, shift)
-    file.write(userInput + " ")
+    file.write(userInput + " \n")
 
 
 def updatemaster():
     print("updated master")
 
 
-def update_account():
-    print("updated account")
+def search(file, account):
+    file.seek(os.SEEK_SET, os.SEEK_END)  # find the end
+    end = file.tell()
+    file.seek(0)  # reset to beginning of file
+    shift = file.readline()
+    temp = file.readline()
+    while file.tell() < end:
+        line = file.readline()
+        temp = line.split()[0]
+        temp = decrypt(temp, shift)
+        if temp == account:
+            return line
+    return "0"
+
+
+def update_account(file):
+    print("What account do you want to update?")
+    command = input()
+    found = search(file, command)
+    if not found == "0":
+        print("What do you want to update?")
+        print("(a) Nickname")
+        print("(b) Username")
+        print("(c) Password")
+        command = input()
+        if command == "a":
+            updateNick()
+        elif command == "b":
+            updateUser()
+        elif command == "c":
+            updatePass()
+        else:
+            print("Error: Invalid Option")
+    else:
+        print("Error: Account not found")
+
+
+def updateNick():
+    return 0
+
+
+def updateUser():
+    return 0
+
+
+def updatePass():
+    return 0
--- Main.py
+++ Main.py
@@ -1,13 +1,16 @@
-import os.path
+import os
 from Commands import *
 from Coder import *
 
 # Open file or create file to store info
-data = open('DO-NOT-DELETE.txt', mode='r+')
+if os.path.isfile('DO-NOT-DELETE.txt'):
+    data = open('DO-NOT-DELETE.txt', mode='r+')
+else:
+    data = open('DO-NOT-DELETE.txt', mode='x+')
 
 
 def createshift():
-    newshift = input("Enter a number: ")
+    newshift = input("Enter a number between 1 and 100: ")
     data.write(newshift + "\n")
     return newshift
 
@@ -23,8 +26,8 @@ if os.path.getsize('DO-NOT-DELETE.txt') == 0:
     shift = createshift()
     create_master_pass(shift)
 
-
 # Ask for shift and master pass
+data.seek(0)
 shift = data.readline()
 password = data.readline().rstrip('\n')
 password = decrypt(password, shift)
@@ -34,17 +37,19 @@ while (userInput != password):
     userInput = userInput
     if userInput != password:
         print("Wrong pass. Try again: ")
+    # ADD OPTION TO CHANGE PASS
+
 
 command = displaymenu()
 while command != "e":
     if command == "a":
-        retrieve_account()
+        retrieve_account(data, shift)
     elif command == "b":
         addaccount(data, shift)
     elif command == "c":
         updatemaster()
     elif command == "d":
-        update_account()
+        update_account(data)
     else:
         print("Invalid Option")
     command = displaymenu()
