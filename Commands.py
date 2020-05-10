6ece365908846fac09a8c1769144574c58d240a6
Giao Pham <giaopham99@yahoo.com>
2020-05-09 23:06:44 -0400

    added with statements and finished updatemaster

--- Commands.py
+++ Commands.py
@@ -6,56 +6,77 @@ def displaymenu():
     print("\nPick an Option:")
     print("(a) Retrieve an Account Log-in")
     print("(b) Add an Account Log-in")
-    print("(c) Update Master Password or shift")
-    print("(d) Update an Account")
-    print("(e) List all Accounts")
+    print("(c) Update Master Password")
+    print("(d) Update shift")
+    print("(e) Update an Account")
+    print("(f) List all Accounts")
     print("(q) Quit")
     return input()
 
 
-def retrieve_account(file, shift):
+def retrieve_account(shift):
+    with open("DO-NOT-DELETE.txt") as data:
+        end = findEnd(data)
+        data.readline()
+        data.readline()
+        if data.tell() == end:
+            return print("Error: You have no accounts logged.")
+        account = input("What account are you looking for? ")
+        line = search(data, account)
+        if line == "0":
+            print("Error: Nickname for account does not exist.")
+        else:
+            line = line.split()
+            line = [decrypt(word, shift) for word in line]
+            print("Account: " + line[0])
+            print("Username: " + line[1])
+            print("Password: " + line[2])
+
+
+def addaccount(shift):
+    with open("DO-NOT-DELETE.txt", "a") as data:
+        userInput = input("Nickname of Account: ")
+        userInput = encrypt(userInput.strip(), shift)
+        data.write(userInput + " ")
+        userInput = input("Username: ")
+        userInput = encrypt(userInput.strip(), shift)
+        data.write(userInput + " ")
+        userInput = input("Password: ")
+        userInput = encrypt(userInput.strip(), shift)
+        data.write(userInput + " \n")
+
+
+def findEnd(file):
     file.seek(os.SEEK_SET, os.SEEK_END)  # find the end
     end = file.tell()
     file.seek(0)  # reset to beginning of file
-    file.readline()
-    file.readline()
-    pos = file.tell()
-    if pos == end:
-        return print("Error: You have no accounts logged.")
-    account = input("What account are you looking for? ")
-    line = search(file, account)
-    if line == "0":
-        print("Error: Nickname for account does not exist.")
-    else:
-        line = line.split()
-        line = [decrypt(word, shift) for word in line]
-        print("Account: " + line[0])
-        print("Username: " + line[1])
-        print("Password: " + line[2])
+    return end
 
 
-def addaccount(file, shift):
-    userInput = input("Nickname of Account: ")
-    userInput = encrypt(userInput, shift)
-    file.write(userInput + " ")
-    userInput = input("Username: ")
-    userInput = encrypt(userInput, shift)
-    file.write(userInput + " ")
-    userInput = input("Password: ")
-    userInput = encrypt(userInput, shift)
-    file.write(userInput + " \n")
+def updatemaster(newpass, shift):
+    with open("DO-NOT-DELETE.txt", "r+") as data:
+        with open("TEMP.txt", "w+") as copy:
+            end = findEnd(data)
+            while data.tell() < end:
+                copy.write(data.readline())
+    with open("DO-NOT-DELETE.txt", "w+") as data:
+        with open("TEMP.txt", "r+") as copy:
+            end = findEnd(copy)
+            data.write(copy.readline())  # copy shift over
+            copy.readline()
+            data.write(decrypt(newpass, shift) + "\n")  # new pass
+            while copy.tell() < end:
+                data.write(copy.readline())
 
 
-def updatemaster():
-    print("updated master")
+def updateshift():
+    return 0
 
 
 def search(file, account):
-    file.seek(os.SEEK_SET, os.SEEK_END)  # find the end
-    end = file.tell()
-    file.seek(0)  # reset to beginning of file
+    end = findEnd(file)
     shift = file.readline()
-    temp = file.readline()
+    file.readline()
     while file.tell() < end:
         line = file.readline()
         temp = line.split()[0]
@@ -65,26 +86,27 @@ def search(file, account):
     return "0"
 
 
-def update_account(file):
-    print("What account do you want to update?")
-    command = input()
-    found = search(file, command)
-    if not found == "0":
-        print("What do you want to update?")
-        print("(a) Nickname")
-        print("(b) Username")
-        print("(c) Password")
+def update_account():
+    with open("DO-NOT-DELETE.txt", "r+") as data:
+        print("What account do you want to update?")
         command = input()
-        if command == "a":
-            updateNick()
-        elif command == "b":
-            updateUser()
-        elif command == "c":
-            updatePass()
+        found = search(data, command)
+        if not found == "0":
+            print("What do you want to update?")
+            print("(a) Nickname")
+            print("(b) Username")
+            print("(c) Password")
+            command = input()
+            if command == "a":
+                updateNick()
+            elif command == "b":
+                updateUser()
+            elif command == "c":
+                updatePass()
+            else:
+                print("Error: Invalid Option")
         else:
-            print("Error: Invalid Option")
-    else:
-        print("Error: Account not found")
+            print("Error: Account not found")
 
 
 def updateNick():
--- Main.py
+++ Main.py
@@ -5,51 +5,62 @@ from Coder import *
 # Open file or create file to store info
 if os.path.isfile('DO-NOT-DELETE.txt'):
     data = open('DO-NOT-DELETE.txt', mode='r+')
+    copy = open('TEMP.txt', mode='r+')
 else:
     data = open('DO-NOT-DELETE.txt', mode='x+')
+    copy = open('TEMP.txt', mode='x+')
 
 
 def createshift():
     newshift = input("Enter a number between 1 and 100: ")
-    data.write(newshift + "\n")
+    with open("DO-NOT-DELETE.txt", "w") as data:
+        data.write(newshift + "\n")
     return newshift
 
 
 def create_master_pass(shift):  # Create a Master Password
     newpass = input("Enter a master password: ")
     newpass = encrypt(newpass, shift)  # encrypt the master password
-    data.write(newpass + "\n")
+    with open("DO-NOT-DELETE.txt", "a") as data:
+        data.write(newpass + "\n")
 
 
+data.close()
+copy.close()
 # If User is new to the program
-if os.path.getsize('DO-NOT-DELETE.txt') == 0:
+if os.path.getsize("DO-NOT-DELETE.txt") == 0:
     shift = createshift()
     create_master_pass(shift)
 
 # Ask for shift and master pass
-data.seek(0)
-shift = data.readline()
-password = data.readline().rstrip('\n')
-password = decrypt(password, shift)
+shift = 0
+with open("DO-NOT-DELETE.txt") as data:
+    shift = data.readline()
+    password = data.readline().strip('\n')
+    password = decrypt(password, shift)
 userInput = ""
 while (userInput != password):
     userInput = input("What is the password? ")
-    userInput = userInput
     if userInput != password:
         print("Wrong pass. Try again: ")
     # ADD OPTION TO CHANGE PASS
 
 
 command = displaymenu()
-while command != "e":
+while command != "q":
     if command == "a":
-        retrieve_account(data, shift)
+        retrieve_account(shift)
     elif command == "b":
-        addaccount(data, shift)
+        addaccount(shift)
     elif command == "c":
-        updatemaster()
+        userInput = input("New Password: ")
+        updatemaster(userInput, shift)
     elif command == "d":
-        update_account(data)
+        updateshift()
+    elif command == "e":
+        update_account()
+    elif command == "f":
+        listall()
     else:
         print("Invalid Option")
     command = displaymenu()
